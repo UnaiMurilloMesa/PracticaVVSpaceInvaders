5a299f3ad830b3e6733bbf4fd54f857f2af970e5
Alberto <amozosk@gmail.com>
2025-10-24 17:15:07 +0200

    Carpetas para visibilidad

---
+++ src/test/java/space_invaders.sprites/KeyEventsTest.java
@@ -0,0 +1,11 @@
+package space_invaders.sprites;
+
+import org.junit.jupiter.api.Test;
+
+public class KeyEventsTest {
+    @Test
+    void test() {
+        Player player = new Player();
+        player.dx;
+    }
+}
